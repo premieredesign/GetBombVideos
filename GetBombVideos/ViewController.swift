e74d257a03188d2299733ae9b289e091864de02c
Clinton Johnson <iamcmj@icloud.com>
2018-11-07 20:04:05 -0500

    Initial Commit

---
+++ GetBombVideos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetBombVideos
+//
+//  Created by Clinton Johnson on 11/7/18.
+//  Copyright © 2018 Clinton Johnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
