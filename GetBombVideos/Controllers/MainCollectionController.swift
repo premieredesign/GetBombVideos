5917b41d05ade83ab012a0b5c018d6b35b53b1ed
Clinton Johnson <iamcmj@icloud.com>
2018-11-07 20:31:22 -0500

    Modified AppDelegate to render custom view controller programmatically

--- GetBombVideos/Controllers/MainCollectionController.swift
+++ GetBombVideos/Controllers/MainCollectionController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainCollectionController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .red
     }
 
 
